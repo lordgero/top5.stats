99d76d4c2e45fd11d0c09558ba3b49f6f9452b6f
Antoine Gerard <angerard@devinci.fr>
2016-02-13 20:40:44 +0100

    Added function for JSON parsing in HomeController.java

--- top5stats/src/main/java/top5/rocket/stats/HomeController.java
+++ top5stats/src/main/java/top5/rocket/stats/HomeController.java
@@ -1,9 +1,18 @@
 package top5.rocket.stats;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+import java.nio.charset.Charset;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -21,11 +30,14 @@ public class HomeController {
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
+	 * @throws IOException 
+	 * @throws JSONException 
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
+	public String home(Locale locale, Model model) throws JSONException, IOException {
 		logger.info("Welcome home! The client locale is {}.", locale);
-		
+		JSONObject json = readJsonFromUrl("http://logs.tf/json_search?player=76561198043755694");
+		System.out.println(json.toString());
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
@@ -34,6 +46,27 @@ public class HomeController {
 		model.addAttribute("serverTime", formattedDate );
 		
 		return "home";
+	}
+	
+	public static JSONObject readJsonFromUrl(String url) throws JSONException, IOException{
+		InputStream is = new URL(url).openStream();
+		try{
+			BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+			String jsonTxt = readAll(rd);
+			JSONObject json = new JSONObject(jsonTxt);
+			return json;
+		}finally{
+			is.close();
+		}
+	}
+	
+	public static String readAll(Reader rd) throws IOException{
+		StringBuilder sb = new StringBuilder();
+		int cp;
+		while((cp = rd.read())!=-1){
+			sb.append((char)cp);
+		}
+		return sb.toString();
 	}
 	
 }
